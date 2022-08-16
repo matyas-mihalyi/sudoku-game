b55183aeaf9eabbde08a21b057d2fa5f6e125b97
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2022-08-16 21:23:38 +0200

    refactor: remove obsolete component method from cell component

--- src/app/sudoku-cell/sudoku-cell.component.ts
+++ src/app/sudoku-cell/sudoku-cell.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ElementRef, AfterViewInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
 import { SudokuService } from '../sudoku.service';
 
 @Component({
@@ -6,12 +6,11 @@ import { SudokuService } from '../sudoku.service';
   templateUrl: './sudoku-cell.component.html',
   styleUrls: ['./sudoku-cell.component.sass']
 })
-export class SudokuCellComponent implements OnInit, AfterViewInit {
+export class SudokuCellComponent implements OnInit {
 
   @Input () number: number|undefined;
   @Input () row!: number;
   @Input () column!: number;
-  @Input () focused: boolean = false;
 
   @Output () sendIndices = new EventEmitter()
 
@@ -24,20 +23,17 @@ export class SudokuCellComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
   }
 
-  ngAfterViewInit() : void {
-  }
-
   public handleInput = (input: Event) => {
     const inputField = (input.target as HTMLInputElement);
     const inputValue = inputField.value;
     const lastInputValue = inputValue[inputValue.length - 1];
 
-    inputField.value = this.convertValue(lastInputValue) || "";
+    inputField.value = this.convertInputValue(lastInputValue) || "";
 
     this.gameService.updateCell(this.row, this.column, inputField.value);
   }
   
-  private convertValue = (input: string) => {
+  private convertInputValue = (input: string) => {
     return input === "0" ?
     ""
     :
@@ -52,26 +48,4 @@ export class SudokuCellComponent implements OnInit, AfterViewInit {
 
   public applyFocus = () => this.cell.nativeElement.focus();
 
-  private removeFocus = () => this.cell.nativeElement.blur();
-
-
-  /* 
-  # handleNav
-  
-  this.removeFocus()
-
-  emitDirection
-
-
-  # in Parent
-
-  1. receive direction & index
-  2. find next index
-  3. call method on viewchild
-  
-  
-  
-  
-  
-  */
 }
