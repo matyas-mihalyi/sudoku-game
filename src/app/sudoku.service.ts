33af5471865c1c348cb641e1e111b6b835499dc5
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2022-08-17 17:09:59 +0200

    feat: addbuttons to request more or less clues

--- src/app/sudoku.service.ts
+++ src/app/sudoku.service.ts
@@ -10,7 +10,17 @@ import { Sudoku } from './types';
 
 export class SudokuService {
 
-  public startingSudoku = generateSudoku(1);
+  private numberOfCellsToRemove = 2;
+
+  private maxNumberOfCellsToRemove = 64;
+
+  private minNumberOfCellsToRemove = 1;
+
+  public upperClueLimitReached = new BehaviorSubject<boolean>(false);
+  
+  public lowerClueLimitReached = new BehaviorSubject<boolean>(false);;
+
+  public startingSudoku = generateSudoku(this.numberOfCellsToRemove);
 
   public sudoku = new BehaviorSubject<Sudoku>(JSON.parse(JSON.stringify(this.startingSudoku)));
 
@@ -23,9 +33,8 @@ export class SudokuService {
   public updateCell = (row: number, col: number, inputValue: string) => {
     const updatedSudoku = this.sudoku.value;
     const value = this.convertInputValue(inputValue)
-    
-    updatedSudoku[row][col] = value;
-    
+    updatedSudoku[row][col] = value;    
+
     this.sudoku.next(updatedSudoku);
   }
 
@@ -49,8 +58,38 @@ export class SudokuService {
   private checkSudokuValidity = (sudoku: Sudoku) => validateSudoku(sudoku);
 
   public createNewSudoku () {
-    this.startingSudoku = generateSudoku(1);
+    this.startingSudoku = generateSudoku(this.numberOfCellsToRemove);
     this.sudoku.next(JSON.parse(JSON.stringify(this.startingSudoku)))
+  }
+  
+  private removeLessCells () {
+    if (this.numberOfCellsToRemove > this.minNumberOfCellsToRemove) {
+      this.numberOfCellsToRemove = this.numberOfCellsToRemove - 1;
+      this.lowerClueLimitReached.next(false);
+    } 
+    if (this.numberOfCellsToRemove === this.minNumberOfCellsToRemove) {
+      this.upperClueLimitReached.next(true);
+    }
+  }
+  
+  private removeMoreCells () {
+    if (this.numberOfCellsToRemove < this.maxNumberOfCellsToRemove) {
+      this.numberOfCellsToRemove = this.numberOfCellsToRemove + 1;
+      this.upperClueLimitReached.next(false);
+    } 
+    if (this.numberOfCellsToRemove === this.maxNumberOfCellsToRemove) {
+      this.lowerClueLimitReached.next(true);
+    }
+  }
+
+  public createSudokuWithMoreClues () {
+    this.removeLessCells();
+    this.createNewSudoku();
+  }
+
+  public createSudokuWithLessClues () {
+    this.removeMoreCells();
+    this.createNewSudoku();
   }
 
 }
