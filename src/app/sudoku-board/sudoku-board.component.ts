a4485a6c0245e38948390f707a4b666cc7ac2e98
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2022-08-18 08:46:02 +0200

    refactor: move navigation logic into separate service

--- src/app/sudoku-board/sudoku-board.component.ts
+++ src/app/sudoku-board/sudoku-board.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 
-import { AnimationService } from '../animation.service';
-import { SudokuService } from '../sudoku.service';
-import { ArrayOf9Elements, CellIndices, Direction, Sudoku } from '../types';
+import { AnimationService } from '../services/animation.service';
+import { NavigationService } from '../services/navigation.service';
+import { SudokuService } from '../services/sudoku.service';
+import { CellIndices, Sudoku } from '../types';
 
 
 @Component({
@@ -14,19 +15,21 @@ export class SudokuBoardComponent implements OnInit {
 
   public sudoku: Sudoku = this.gameService.startingSudoku;
 
-  private focusedCellIndices: CellIndices = { row: -1, column: -1 }
-
   public sudokuIsSolved = false;
 
   public maxCluesReached = false;
   
   public minCluesReached = false;
 
-  @ViewChildren("cell") cells!: QueryList<any>
+  @ViewChildren("cell")
+  set cells(value: QueryList<any>) {
+    setTimeout(()=> this.navigationService.cells = value)
+  }
 
   constructor(
     private gameService : SudokuService,
-    private animationService: AnimationService
+    private animationService: AnimationService,
+    private navigationService: NavigationService
   ) { 
   }
   
@@ -63,7 +66,7 @@ export class SudokuBoardComponent implements OnInit {
   }
 
   disableInput(): void {
-    this.cells.toArray().forEach(cell => cell.disableCell());
+    this.navigationService.cells.toArray().forEach(cell => cell.disableCell());
   }
 
   handleNewSudokuRequest():void {
@@ -87,81 +90,8 @@ export class SudokuBoardComponent implements OnInit {
     this.animationService.animate("anotherOne");
   }
 
-  handleNavigation = (e: KeyboardEvent) => {
-    if (this.isArrowKey(e)) {
-      this.focusNextCell(e, this.focusedCellIndices.row, this.focusedCellIndices.column)
-    }
-  }
+  handleNavigation = (e: KeyboardEvent) => this.navigationService.handleNavigation(e); 
 
-  isArrowKey (e: KeyboardEvent) {
-    const arrowKeys = ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft"]
-    return arrowKeys.some(key => e.key === key);  
-  }
-
-  updateFocusedCell (newIndices: CellIndices) {
-    this.focusedCellIndices = newIndices;
-  }
-
-  blurCurrentCell () {
-    const index = this.focusedCellIndices.row * this.focusedCellIndices.column;
-    this.cells.toArray()[index].removeFocus();
-  }
-
-  focusNextCell (event: KeyboardEvent , currentRow:number, currentCol:number) {
-    const nextCellIndex = this.findNextCellIndex[event.key as Direction](currentRow, currentCol);
-    const nextCell = this.cells.toArray()[nextCellIndex];
-    nextCell.applyFocus();
-  }
-
-  findNextCellIndex = {
-    "ArrowUp": (currentRow:number, currentCol:number) => this.findCellIndexUp(currentRow, currentCol),
-    "ArrowRight": (currentRow:number, currentCol:number) => this.findCellIndexRight(currentRow, currentCol),
-    "ArrowDown": (currentRow:number, currentCol:number) => this.findCellIndexDown(currentRow, currentCol),
-    "ArrowLeft": (currentRow:number, currentCol:number) => this.findCellIndexLeft(currentRow, currentCol),
-  }
-
-  findCellIndexRight (currentRow: number, currentCol: number): number {
-    const nextIndexInRow = this.getNextAvailableIndex(this.sudoku[currentRow], currentCol);
-    const nextIndexInSudoku = nextIndexInRow + (currentRow * 9);
-    return nextIndexInSudoku; 
-  }
+  updateFocusedCell = (newIndices: CellIndices) => this.navigationService.updateFocusedCell(newIndices);
   
-  findCellIndexLeft (currentRow: number, currentCol: number): number {
-    const previousIndexInRow = this.getPreviousAvailableIndex(this.sudoku[currentRow], currentCol);
-    const previousIndexInSudoku = previousIndexInRow + (currentRow * 9);
-    return previousIndexInSudoku; 
-  }
-  
-  findCellIndexDown (currentRow: number, currentCol: number): number {
-    const column = this.getColumnFromSudoku(currentCol);
-    const nextIndexInColumn = this.getNextAvailableIndex(column, currentRow);
-    const nextIndexInSudoku = nextIndexInColumn * 9 + currentCol;
-    return nextIndexInSudoku; 
-  } 
-
-  findCellIndexUp (currentRow: number, currentCol: number): number {
-    const column = this.getColumnFromSudoku(currentCol);
-    const previousIndexInColumn = this.getPreviousAvailableIndex(column, currentRow);
-    const previousIndexInSudoku = previousIndexInColumn * 9 + currentCol;
-    return previousIndexInSudoku; 
-  } 
-
-  getNextAvailableIndex = (arr: ArrayOf9Elements<number|undefined>, currentIndex: number): number => {
-    return currentIndex + 1 < arr.length ?
-    currentIndex + 1
-    :
-    0
-  }
-    
-  getPreviousAvailableIndex = (arr: ArrayOf9Elements<number|undefined>, currentIndex: number): number => {
-    return currentIndex - 1 >= 0 ?
-    currentIndex - 1
-    :
-    arr.length - 1
-  }
-  
-  getColumnFromSudoku = (columnIndex: number) => {
-    return this.sudoku.map(row => row[columnIndex]) as ArrayOf9Elements<number|undefined>;
-  }
-
 }
