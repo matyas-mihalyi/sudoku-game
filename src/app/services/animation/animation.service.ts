60330333a915ff75a6af04c13071df6ccf9c9086
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2022-08-23 10:27:30 +0200

    refactor: make isAnimated service variable private

--- src/app/services/animation/animation.service.ts
+++ src/app/services/animation/animation.service.ts
@@ -9,20 +9,20 @@ import { assets } from './animation-assets';
 })
 export class AnimationService {
   
-  public isAnimated = new BehaviorSubject<boolean>(false)
-
   constructor() { }
-
+  
   public getImage = () => this.img.asObservable();
   
   public getState = () => this.isAnimated.asObservable();
-
+  
   public animate (type: AnimationType) {
     const img = assets[type]();
     this.setImage(img);
     this.isAnimated.next(true);
     setTimeout(()=> this.isAnimated.next(false), 3000);
   }
+
+  private isAnimated = new BehaviorSubject<boolean>(false);
 
   private img = new BehaviorSubject<{src:string, alt:string}>({src:"", alt:""})
   
